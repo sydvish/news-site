3894e70d88dd98147b3325b596ff4559f4c13624
sydney Volk <sydneyvolk@sydneys-MacBook-Pro.local>
2019-03-13 14:32:59 -0700

    commit

---
+++ public/app.js
@@ -0,0 +1,75 @@
+// We'll be rewriting the table's data frequently, so let's make our code more DRY
+// by writing a function that takes in 'animals' (JSON) and creates a table body
+function displayResults(data) {
+  console.log(data.results);
+  var results = data.results;
+  // First, empty the table
+  $("card").empty();
+
+  for (var i = 0; i < results.length; i++) {
+
+    var tr = $("<div>", { class: "card-body" }).append(
+      $("<div>", { class: "card-title" }).text(results[i].title),
+      $("<a>", { class: "card-text" })
+        .attr({ href: results[i].link, target: "_blank" })
+        .text("link"),
+      $("<a>", {class: "button"})
+    );
+
+    $("#results").append(tr);
+  };
+
+}
+
+  
+// Bonus function to change "active" header
+function setActive(selector) {
+  // remove and apply 'active' class to distinguish which column we sorted by
+  $("th").removeClass("active");
+  $(selector).addClass("active");
+  console.log(selector);
+}
+
+// Function to clear article data from the screen by clicking clear button
+function clearArticles(data) {
+  $("th").removeClass("active");
+  // $(selector).addClass("active");
+  console.log(data);
+}
+
+// 1: On Load
+// ==========
+
+// First thing: ask the back end for json with all animals
+$.getJSON("/all/db", function(data) {
+  // Call our function to generate a table body
+  displayResults(data);
+});
+
+// 2: Button Interactions
+// ======================
+
+// When user clicks the scrape new articles button, display table sorted by weight
+$("#button1").on("click", function() {
+  // Set new column as currently-sorted (active)
+  setActive(".card-body");
+
+  // Do an api call to the back end for json with all animals sorted by weight
+  $.getJSON("/all/db", function(data) {
+    // Call our function to generate a table body
+    displayResults(data);
+    console.log(data);
+  });
+});
+
+// When this button is clicked we want all the articles cleared and
+$("#button2").on("click", function() {
+  clearArticles(results);
+  console.log(results);
+
+  $.getJSON("/all/db", function(data) {
+    // Call our function to generate a table body
+    clearArticles(data);
+    console.log(data);
+  });
+});
---
+++ server.js
@@ -0,0 +1,140 @@
+// Using the tools and techniques you learned so far,
+// you will scrape a website of your choice, then place the data
+// in a MongoDB database. Be sure to make the database and collection
+// before running this exercise.
+
+// Consult the assignment files from earlier in class
+// if you need a refresher on Cheerio.
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
+// Initialize Express
+var app = express();
+
+app.use(express.static("public"));
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.sendFile("/Users/sydneyvolk/code/AZCHA201901FSF2-FT/18-mongo-mongoose/01-Activities/11-Scraping-into-a-db/Unsolved/index.html");
+});
+
+// TODO: make two more routes
+
+// Route 1
+// =======
+// This route will retrieve all of the data
+// from the scrapedData collection as a json (this will be populated
+// by the data you scrape using the next route)
+
+app.get("/all", function(req, res) {
+  axios.get("https://www.npr.org/").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    // An empty array to save the data that we'll scrape
+    var results = [];
+
+    $("div.story-text").each(function(i, element) {
+      // Save the text of the element in a "title" variable
+      var title = $(element)
+        .find("h3.title")
+        .text();
+      var link = $(element)
+        .find("a")
+        .attr("href");
+
+      // Save these results in an object that we'll push into the results array we defined earlier
+      results.push({
+        title: title,
+        link: link
+      });
+    });
+
+    // Log the results once you've looped through each of the elements found with cheerio
+    console.log(results);
+  });
+});
+
+// Route 2
+// =======
+// When you visit this route, the server will
+// scrape data from the site of your choice, and save it to
+// MongoDB.
+// TIP: Think back to how you pushed website data
+// into an empty array in the last class. How do you
+// push it into a MongoDB collection instead?
+
+app.get("/all/db", function(req, res) {
+  console.log("listening");
+  axios.get("https://www.npr.org/").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    // An empty array to save the data that we'll scrape
+    var results = [];
+
+
+    $("div.story-text").each(function(i, element) {
+      // Save the text of the element in a "title" variable
+      var title = $(element)
+        .find("h3.title")
+        .text();
+      var link = $(element)
+        .find("a")
+        .attr("href");
+      console.log(title);
+      console.log(link);
+      if (title != undefined && link != undefined) {
+        // Save these results in an object that we'll push into the results array we defined earlier
+        results.push({
+          title: title,
+          link: link
+        });
+      }
+    });
+
+    // Log the results once you've looped through each of the elements found with cheerio
+    db.scrapedData.insert({ results }, function (error, data){
+      res.json(data)
+      // displayResults(data);
+    })
+
+  });
+});
+
+// function displayResults(data) {
+//   // First, empty the table
+//   $("tbody").empty();
+
+
+//   // Then, for each entry of that json...
+//   data.forEach(function(data) {
+//     // Append each of the animal's properties to the table
+//     var tr = $("<tr>").append(
+//       $("<td>").text(results[i].title),
+//       $("<td>").text(results[i].link),
+//     );
+
+//     $("#results").append(tr);
+//   });
+// }
+
+/* -/-/-/-/-/-/-/-/-/-/-/-/- */
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
